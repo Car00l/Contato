fe7971151e2380e1cb6e4532f604314fe403c85b
4ngelo00 <gabrielangelo0320@gmail.com>
2023-03-09 22:14:27 -0300

    salvar

--- API/produto/model/save.php
+++ API/produto/model/save.php
@@ -1,38 +1,45 @@
 <?php
-
-// instancia com o banco de dados
 include('../../conexao/conn.php');
 
+$NOME_PRODUTO = $_POST['NOME_PRODUTO']; //nome do produto vindo de um formulário, por exemplo
+$sql = "SELECT NOME_PRODUTO FROM PRODUTO WHERE NOME_PRODUTO = $NOME_PRODUTO";
+$stmt->bindParam(':NOME_PRODUTO', $NOME_PRODUTO, PDO::PARAM_STR);
 
-
-try{
-    // gerar a querie de insersao no banco de dados 
+if ($stmt->execute()){
+//verifica se o produto já existe
+if ($stmt->rowCount() < 0) {
+    echo "O produto já existe.";
+} else {
+    try{
+    // gerar a query de inserção no banco de dados 
     $sql = "INSERT INTO PRODUTO (NOME_PRODUTO, MARCA_PRODUTO, FABRICANTE_PRODUTO, VALOR_COMPRA_PRODUTO, VALOR_VENDA_PRODUTO, ESTOQUE_PRODUTO) VALUES (?, ?, ?, ?, ?, ?)"; // colocar ? para deixar mais seguro
-    // preparar a querie para gerar objetos de insersao no banco de dados
+    // preparar a query para gerar objetos de inserção no banco de dados
 
-    $stmt = $pdo->prepare($sql); // atribuindo para ver se existe
+    $stmt = $pdo->prepare($sql); // corrigir a referência à conexão com o banco de dados
 
     // se existir requerir os valores
     $stmt->execute([
-        $_REQUEST['NOME_PRODUTO'],
-        $_REQUEST['MARCA_PRODUTO'],
-        $_REQUEST['FABRICANTE_PRODUTO'],
-        $_REQUEST['VALOR_COMPRA_PRODUTO'],
-        $_REQUEST['VALOR_VENDA_PRODUTO'],
-        $_REQUEST['ESTOQUE_PRODUTO']
+        $_REQUEST['NOME_PRODUTO'], 
+        $_REQUEST['MARCA_PRODUTO'], 
+        $_REQUEST['FABRICANTE_PRODUTO'], 
+        $_REQUEST['VALOR_COMPRA_PRODUTO'], 
+        $_REQUEST['VALOR_VENDA_PRODUTO'], 
+        $_REQUEST['ESTOQUE_PRODUTO'] 
     ]);
 
-    // tranforma os dados em um array
+    // transforma os dados em um array
     $dados = array(
         'type' => 'success',
         'mensagem' => 'Registro salvo com sucesso!'
     );
     // se nao existir mostrar erro
-}catch (PDOExeception $e){
+}catch (PDOException $e){ // corrigir a grafia de PDOException
     $dados = array(
         'type' => 'error',
-        'mensagem' => 'Erro ao salvar o registro:' .$e
+        'mensagem' => 'Erro ao salvar o registro:' .$e->getMessage() // adicionar o método getMessage() para exibir a mensagem de erro
     );
+}
+}
 }
 
 echo json_encode($dados);
